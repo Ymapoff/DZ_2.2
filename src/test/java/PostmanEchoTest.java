a301b973728b1d5be9e31dd44f60dd7cdb28899c
Ymapoff <kumar13-05@mail.ru>
2023-06-22 18:19:54 +0300

    com

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -15,7 +15,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("tests"))
+                .body("data", equalTo("test"))
         ;
     }
 }
